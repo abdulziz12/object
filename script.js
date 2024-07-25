14c85301d0cda2f87700b1ab05ccf2927eeaa302
abdulziz12 <abdurashidov200930@gmail.com>
2024-07-25 15:58:50 +0500

    dfcdcxcx
    cvxcvx

---
+++ script.js
@@ -0,0 +1,11 @@
+let ism = prompt('ism kiriting')
+let Spaceid = prompt('Space id kiriting')
+let spacePassword = prompt('Space pasword kiriting')
+
+
+let foydalanuvchi = {
+    name: ism,
+    id: Spaceid,   
+    possword:  spacePassword,
+};
+console.log(foydalanuvchi)
